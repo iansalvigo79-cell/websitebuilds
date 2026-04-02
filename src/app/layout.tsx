a4c2ad50172a2938369b1aca49ca7a64a0051f5e
businessman12598-ctrl <businessman12598@gmail.com>
2026-04-02 05:57:38 -0400

    added GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,6 +65,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-96F0345M6R"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-96F0345M6R');
+            `,
+          }}
+        />
         {/* Google Fonts for heading style */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
